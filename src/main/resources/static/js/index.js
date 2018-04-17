5a502d0d51f1db88309b667255ed627dc6741a8c
xly <380316264@qq.com>
2018-04-17 13:43:12 +0800

    添加查询分页

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,74 @@
 var layer = layui.layer;
+var imgMax = 3;
+function imgErr(obj){
+    var imgIndex = parseInt(Math.random()*imgMax+1);
+    obj.src="img/default/" + imgIndex + ".jpg";
+    obj.onerror=null;
+}
+
+layui.use('laypage', function(){
+    var laypage = layui.laypage;
+    
+    // 查询博文列表
+    var url = 'api/article/list';
+    var data = {};
+    $.ajax({
+        type : "post",
+        url : url,
+        data : data,
+        dataType : 'json',
+        success : function(data) {
+            // 执行一个laypage实例
+            laypage.render({
+              elem: 'articlePage' // 注意，这里的 test1 是 ID，不用加 # 号
+              ,count: data.length // 数据总数，从服务端得到
+              ,limit: 6
+              ,theme: '#006dcc'
+              ,jump: function(obj, first){
+                  // obj包含了当前分页的所有参数，比如：
+                  // console.log(obj.curr); // 得到当前页，以便向服务端请求对应页的数据。
+                  // console.log(obj.limit); // 得到每页显示的条数
+                  
+                  var htm = '';
+                  var curNo = (obj.curr-1)*obj.limit;
+                  for(var i=curNo; i<data.length; i++){
+                      if(i >= obj.curr*obj.limit){
+                          break;
+                      }
+                      
+                      if(i%3 == 0){
+                          htm += '<li class="span4" style="margin-left: 0;">';
+                      }
+                      else
+                      {
+                          htm += '<li class="span4">';
+                      }
+                      
+                      htm += '<div class="thumbnail">';
+                      htm += '<img alt="image not found" src="' + data.titleImgUrl + '" onerror="imgErr(this)" />';
+                      htm += '<div class="caption">';
+                      htm += '<h3>' + data[i].title + '</h3>';
+                      htm += '<p>' + data[i].context + '</p>';
+                      htm += '<p><a class="btn btn-primary" href="#">浏览</a> <a class="btn" href="#">分享</a></p>';
+                      htm += '</div>';
+                      htm += '</div>';
+                      htm += '</li>';
+                  }
+                  
+                  $("#articleList").html(htm);
+                  
+                  // 首次不执行
+                  if(!first){
+                    
+                  }
+              }
+            });
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            layer.msg(XMLHttpRequest.responseText);
+        }
+    });
+});
 
 $(document).ready(function() {
 	$('#summernote').summernote({
@@ -31,46 +101,5 @@ $(document).ready(function() {
 	    });
 	});
 	
-	// 查询博文列表
-	var url = 'api/article/list';
-	var data = {};
-    $.ajax({
-        type : "post",
-        url : url,
-        data : data,
-        dataType : 'json',
-        success : function(data) {
-            var htm = '';
-            for(var i=0; i<data.length; i++){
-                if(i >= 6){
-                    break;
-                }
-                
-                if(i%3 == 0){
-                    htm += '<li class="span4" style="margin-left: 0;">';
-                }
-                else
-                {
-                    htm += '<li class="span4">';
-                }
-                
-                htm += '<div class="thumbnail">';
-                htm += '<img alt="image not found" src="' + data.titleImgUrl + '" onerror="this.src=\'img/sports.jpg;this.onerror=null\'" />';
-                htm += '<div class="caption">';
-                htm += '<h3>' + data[i].title + '</h3>';
-                htm += '<p>' + data[i].context + '</p>';
-                htm += '<p><a class="btn btn-primary" href="#">浏览</a> <a class="btn" href="#">分享</a></p>';
-                htm += '</div>';
-                htm += '</div>';
-                htm += '</li>';
-            }
-            
-            $("#articleList").html(htm);
-            
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            layer.msg(XMLHttpRequest.responseText);
-        }
-    });
 });
 
