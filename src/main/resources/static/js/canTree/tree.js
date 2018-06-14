09b12c1f3b7938557eb38f6d8793eb85ccfae389
xly <380316264@qq.com>
2018-06-14 16:41:42 +0800

    tree big add

--- src/main/resources/static/js/canTree/tree.js
+++ src/main/resources/static/js/canTree/tree.js
@@ -54,7 +54,7 @@ function lerp(a, b, p) {
         return composite;
     }
 
-    window.onload = function () {
+    function treeHead() {
         var canvas = document.getElementById("scratch");
 
         // canvas dimensions
@@ -121,4 +121,78 @@ function lerp(a, b, p) {
         };
 
         loop();
+    }
+    
+    function treeBig() {
+        var canvas = document.getElementById("scratchTree");
+
+        // canvas dimensions
+        var width = parseInt(canvas.style.width);
+        var height = parseInt(canvas.style.height);
+
+        // retina
+        var dpr = window.devicePixelRatio || 1;
+        canvas.width = width * dpr;
+        canvas.height = height * dpr;
+        canvas.getContext("2d").scale(dpr, dpr);
+
+        // simulation
+        var sim = new VerletJS(width, height, canvas);
+        sim.gravity = new Vec2(0, 0);
+        sim.friction = 0.98;
+
+        // entities
+        var tree1 = sim.tree(new Vec2(width / 4, height - 120), 5, 70, 0.95, (Math.PI / 2) / 3);
+        var tree2 = sim.tree(new Vec2(width - width / 4, height - 120), 5, 70, 0.95, (Math.PI / 2) / 3);
+
+        tree2.drawParticles = function (ctx, composite) {
+            var i;
+            for (i = 0; i < composite.particles.length; ++i) {
+                var particle = composite.particles[i];
+                if (particle.leaf) {
+
+                    ctx.beginPath();
+                    ctx.arc(particle.pos.x, particle.pos.y, 15, 0, 2 * Math.PI);
+                    ctx.fillStyle = "#679d7c";
+                    ctx.fill();
+                }
+            }
+        }
+
+        tree2.drawConstraints = function (ctx, composite) {
+            var i;
+
+            ctx.save();
+            ctx.strokeStyle = "#543324";
+            ctx.lineCap = "round";
+
+            for (i = 0; i < composite.constraints.length; ++i) {
+                var constraint = composite.constraints[i];
+                if (!(constraint instanceof DistanceConstraint && typeof constraint.p != "undefined"))
+                    continue;
+
+                ctx.beginPath();
+                ctx.moveTo(constraint.a.pos.x, constraint.a.pos.y);
+                ctx.lineTo(constraint.b.pos.x, constraint.b.pos.y);
+                ctx.lineWidth = lerp(10, 2, constraint.p);
+                ctx.stroke();
+            }
+
+            ctx.restore();
+        }
+
+
+        // animation loop
+        var loop = function () {
+            sim.frame(16);
+            sim.draw();
+            requestAnimFrame(loop);
+        };
+
+        loop();
+    }
+    
+    window.onload = function () {
+        treeHead();
+        treeBig();
     };
